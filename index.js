9bbd51ec0f393fcfe36c19e7419eb42b5e9818e4
Alex Manko <alex@getjerry.com>
2019-09-10 19:50:02 +0300

    Replacing ViewPageAndroid with community version

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { Component } = React;
 const { ViewPropTypes } = ReactNative = require('react-native');
 const createReactClass = require('create-react-class');
 const PropTypes = require('prop-types');
+const ViewPagerAndroid = require('@react-native-community/viewpager');
 const {
   Dimensions,
   View,
@@ -10,7 +11,6 @@ const {
   ScrollView,
   Platform,
   StyleSheet,
-  ViewPagerAndroid,
   InteractionManager,
 } = ReactNative;
 const TimerMixin = require('react-timer-mixin');
@@ -332,7 +332,7 @@ const ScrollableTabView = createReactClass({
     if (!width || width <= 0 || Math.round(width) === Math.round(this.state.containerWidth)) {
       return;
     }
-    
+
     if (Platform.OS === 'ios') {
       const containerWidthAnimatedValue = new Animated.Value(width);
       // Need to call __makeNative manually to avoid a native animated bug. See
